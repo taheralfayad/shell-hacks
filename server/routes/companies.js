9970f676bf01442fea2baeb884df039bb41cad17
Zander Preston <zanpres@gmail.com>
2023-09-16 16:35:48 -0400

    Add new endpoint for getting average score

--- server/routes/companies.js
+++ server/routes/companies.js
@@ -24,6 +24,135 @@ router.get('/', async (req, res) => {
     }
 });
 
+// Aggregate all of the numerical values for Company and map them to calculateScore. Add the results to a new object and return it.
+router.get(`/score`, async (req, res) => {
+    try {
+        const allCompanies = await Company.find();
+
+        if (!allCompanies) {
+            return res.status(404).json({ error: 'Companies not found' });
+        }
+
+        let minMaxCompanyData;
+        await Company.aggregate([
+            {
+                $group: {
+                    _id: null,
+                    maxCarbonEmissions: { $max: '$carbonEmissions' },
+                    minCarbonEmissions: { $min: '$carbonEmissions' },
+                    maxRenewableEnergyUsage: { $max: '$renewableEnergyUsage' },
+                    minRenewableEnergyUsage: { $min: '$renewableEnergyUsage' },
+                    maxWasteGenerated: { $max: '$wasteGenerated' },
+                    minWasteGenerated: { $min: '$wasteGenerated' },
+                    maxMinorityDiversity: { $max: '$minorityDiversity' },
+                    minMinorityDiversity: { $min: '$minorityDiversity' },
+                    maxNonProfitContributions: {
+                        $max: '$nonProfitContributions',
+                    },
+                    minNonProfitContributions: {
+                        $min: '$nonProfitContributions',
+                    },
+                    maxEmployeeTurnover: { $max: '$employeeTurnover' },
+                    minEmployeeTurnover: { $min: '$employeeTurnover' },
+                },
+            },
+        ])
+            .exec()
+            .then((result) => {
+                minMaxCompanyData = result[0];
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+            });
+
+        const mappedCompanies = [];
+        for (let company in allCompanies) {
+            const companyObj = {};
+            companyObj.carbonEmissions = calculateScore(
+                0,
+                100,
+                allCompanies[company].carbonEmissions,
+                minMaxCompanyData.minCarbonEmissions,
+                minMaxCompanyData.maxCarbonEmissions,
+                true
+            );
+            companyObj.renewableEnergyUsage = calculateScore(
+                0,
+                100,
+                allCompanies[company].renewableEnergyUsage,
+                minMaxCompanyData.minRenewableEnergyUsage,
+                minMaxCompanyData.maxRenewableEnergyUsage
+            );
+            companyObj.wasteGenerated = calculateScore(
+                0,
+                100,
+                allCompanies[company].wasteGenerated,
+                minMaxCompanyData.minWasteGenerated,
+                minMaxCompanyData.maxWasteGenerated,
+                true
+            );
+            companyObj.minorityDiversity = calculateScore(
+                0,
+                100,
+                allCompanies[company].minorityDiversity,
+                minMaxCompanyData.minMinorityDiversity,
+                minMaxCompanyData.maxMinorityDiversity
+            );
+            companyObj.nonProfitContributions = calculateScore(
+                0,
+                100,
+                allCompanies[company].nonProfitContributions,
+                minMaxCompanyData.minNonProfitContributions,
+                minMaxCompanyData.maxNonProfitContributions
+            );
+            companyObj.employeeTurnover = calculateScore(
+                0,
+                100,
+                allCompanies[company].employeeTurnover,
+                minMaxCompanyData.minEmployeeTurnover,
+                minMaxCompanyData.maxEmployeeTurnover,
+                true
+            );
+
+            mappedCompanies.push(companyObj);
+        }
+
+        let result = mappedCompanies.reduce((sumCompany, nextCompany) => {
+            return {
+                carbonEmissions:
+                    sumCompany.carbonEmissions + nextCompany.carbonEmissions,
+                renewableEnergyUsage:
+                    sumCompany.renewableEnergyUsage +
+                    nextCompany.renewableEnergyUsage,
+                wasteGenerated:
+                    sumCompany.wasteGenerated + nextCompany.wasteGenerated,
+                minorityDiversity:
+                    sumCompany.minorityDiversity +
+                    nextCompany.minorityDiversity,
+                nonProfitContributions:
+                    sumCompany.nonProfitContributions +
+                    nextCompany.nonProfitContributions,
+                employeeTurnover:
+                    sumCompany.employeeTurnover + nextCompany.employeeTurnover,
+            };
+        });
+
+        res.json({
+            carbonEmissions: result.carbonEmissions / mappedCompanies.length,
+            renewableEnergyUsage:
+                result.renewableEnergyUsage / mappedCompanies.length,
+            wasteGenerated: result.wasteGenerated / mappedCompanies.length,
+            minorityDiversity:
+                result.minorityDiversity / mappedCompanies.length,
+            nonProfitContributions:
+                result.nonProfitContributions / mappedCompanies.length,
+            employeeTurnover: result.employeeTurnover / mappedCompanies.length,
+        });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
 // Get a specific company by ID
 router.get('/:stockSymbol', async (req, res) => {
     try {
